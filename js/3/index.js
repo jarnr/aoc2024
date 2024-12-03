19fdca78b171e1f98144dd032c8b8d2a2ef57fd3
John Richmond <jar@jarnr.com>
2024-12-03 16:37:55 -0500

    day 3

--- js/3/index.js
+++ js/3/index.js
@@ -1,13 +1,36 @@
-const partOne = () => {
-
+const partOne = (input) => {
+    const muls = [...input.matchAll(/mul\(\d+,\d+\)/g)].map(match => match[0]);
+    let total = 0;
+    for (const mul of muls) {
+        const args = mul.match(/(\d+),(\d+)/);
+        total += parseInt(args[1]) * parseInt(args[2]);
+    }
+    console.log(`Part 1: ${total}`);
 };
 
-const partTwo = () => {
-    return true;
+const partTwo = (input) => {
+    const instructions = [...input.matchAll(/(mul\(\d+,\d+\)|do\(\)|don't\(\))/g)].map(match => match[0]);
+	
+	let enabled = true;
+	let total = 0;
+	for (const instruction of instructions) {
+		if (instruction === 'do()') {
+			enabled = true;
+		} else if (instruction === "don't()") {
+			enabled = false;
+		} else if (enabled) {
+			const args = instruction.match(/(\d+),(\d+)/);
+			total += parseInt(args[1]) * parseInt(args[2]);
+		}
+	}
+	console.log(`Part 2: ${total}`);
 };
 
 const main = () => {
-	
+    const input = document.body.innerText;
+
+    partOne(input);
+    partTwo(input);
 };
 
 main();
